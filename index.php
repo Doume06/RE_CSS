0dfc19c7ce21a9b12c747d63d5aec8a5fdc55dd9
Bob El Weird <40604587+bobelweird@users.noreply.github.com>
2019-03-12 14:39:30 +0100

    Add files via upload
    new index

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+
+<head>
+
+<link href="/collapse/css/app.min.css" media="screen" rel="stylesheet" type="text/css"/>
+<link href="/collapse/css/rtbatiment.min.css" media="screen" rel="stylesheet" type="text/css"/>
+<script type="text/javascript" src="/collapse/js/app.min.js"></script>
+<style>
+
+
+</style>
+
+</head>
+
+<body>
+<p>
+
+<div class="content">
+
+    <div class="container">
+        <div class="row">
+           
+               <div class="box-collapse">
+                    <h3 class="collapse-header" data-toggle="collapse" data-target="#01">
+                        <span class="arrow"></span> <font color='black'>Arrêtés relatifs à l'agrément de demandes de Titre V - Année 2018</font>  <!-- <font color='#9B0027'>2018</font> -->
+                    </h3>
+                    <div class="collapse hide" id="01">
+<li><a href="https://www.bulletin-officiel.developpement-durable.gouv.fr/documents/Bulletinofficiel-0028053/met_20140019_0000_0006.pdf" target="_blank" class="external-link-new-window">Arrêté du 2 octobre 2014</a> relatif à l'agrément de la demande de Titre V relative à la prise en compte du système «Regudis» dans la réglementation thermique 2012 </li> <br />
+
+<li><a href="https://www.bulletin-officiel.developpement-durable.gouv.fr/documents/Bulletinofficiel-0028001/met_20140017_0100_0010.pdf" target="_blank" class="external-link-new-window">Arrêté du 22 août 2014</a> relatif à l'agrément de la demande de titre V relative à la prise en compte du système ERS dans la RT2012 	<br /></p>
+  <ul style="font-size:88%;">
+    <li>Le fichier informatique .dll permettant permettant l'application de l'arrêté du 22 août 2014 relatif à l'agrément de la demande de titre V relative à la prise en compte du système ERS dans la réglementation thermique 2012 a été diffusée aux éditeurs de logiciel et sera donc diffusée par eux avec les mises à jour des logiciels d'application</li>
+  </ul><p></li>
+  <br />	 
+
+
+<li><a href="https://www.bulletin-officiel.developpement-durable.gouv.fr/documents/Bulletinofficiel-0027519/met_20140007_0100_0004.pdf" target="_blank" class="external-link-new-window">Arrêté du 26 mars 2014</a> relatif à l'agrément de la demande de titre V relative à la prise en compte du système « PAC Facteur 7 » </p>
+  <ul style="font-size:88%;">
+    <li>Le fichier informatique .dll permettant permettant l'application de l'arrêté du 26 mars 2014 relatif à l'agrément de la demande de titre V relative à la prise en compte du système « PAC Facteur 7 » dans la réglementation thermique 2012 a été diffusée aux éditeurs de logiciel et sera donc diffusée par eux avec les mises à jour des logiciels d'application</li></ul><p> </li><br />	
+                      
+
+
+                    </div>
+                </div>
+
+
+               <div class="box-collapse">
+                    <h3 class="collapse-header" data-toggle="collapse" data-target="#02">
+                        <span class="arrow"></span> <font color='black'>Arrêtés antérieurs</font>  <!-- <font color='#9B0027'>2018</font> -->
+                    </h3>
+                    <div class="collapse hide" id="02">
+                       <ul>
+                       	<li>
+                       		Arrêté du 7 décembre 2017
+                       	</li>
+                       </ul>
+                      
+
+
+                    </div>
+                </div>
+
+
+</div>
+</div>
+</div>
+
+
+</body>
+
+
+</html>
